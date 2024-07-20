3e31e5ae4e1631182ab84c094e6abe7d78fcb871
Jorgecas71 <jorgecas71@gmail.com>
2024-07-20 18:06:13 +0200

    init project

---
+++ src/test/java/dev/jorge/project/support/app/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jorge.project.support.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
